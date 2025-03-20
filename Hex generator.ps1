5d54ef8b8620bf95416ae965b1462b285edbc3b5
lebetten <lebetten@gmail.com>
2025-03-20 10:07:25 +0100

    Add Hex generator script to generate random hexadecimal numbers

---
+++ Hex generator.ps1
@@ -0,0 +1,4 @@
+﻿$randomNumber = ((1..64 | .{Process{ Get-Random -InputObject ([char[]]"0123456789ABCDEF") }}) -join '')
+$randomNumber = $randomNumber.ToLower()
+$randomNumber
+Read-Host -Prompt "Press Enter to exit"
